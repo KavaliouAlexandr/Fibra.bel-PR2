e4fefba5bbf83f1dedb7353a62db50f804586b4b
Alexandr Kavaliou <80467603+KavaliouAlexandr@users.noreply.github.com>
2024-04-11 21:17:48 +0200

    navbar transition

--- Kontakt/script.js
+++ Kontakt/script.js
@@ -1,14 +1,15 @@
 let lastScrollTop = 0;
+const navbar = document.querySelector(".header");
 
 window.addEventListener("scroll", function() {
     let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
 
     if (currentScroll > lastScrollTop) {
         // Прокрутка вниз
-        document.querySelector(".header").classList.add("hidden");
+        navbar.style.top = "-80px"; // Плавно выезжаем наверх
     } else {
         // Прокрутка вверх
-        document.querySelector(".header").classList.remove("hidden");
+        navbar.style.top = "0"; // Плавно возвращаем навбар на место
     }
 
     lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Для поддержки Safari

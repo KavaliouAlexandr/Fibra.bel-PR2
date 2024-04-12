e9bb325da872964d73c5d5836fa22008bfb85b90
Alexandr Kavaliou <80467603+KavaliouAlexandr@users.noreply.github.com>
2024-04-12 14:50:35 +0200

    Kontakt form

--- html/script.js
+++ html/script.js
@@ -14,3 +14,34 @@ window.addEventListener("scroll", function() {
 
     lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Для поддержки Safari
 }, false);
+
+
+document.querySelector(".contact-form").addEventListener("submit", function(event) {
+    
+    this.reset();
+});
+
+// const AES = require('crypto-js/aes');
+
+// // Ваш токен от Web3Forms
+// const token = "f82c6329-b6b3-40be-8c6b-087136429e11";
+
+// // Ключ для шифрования (может быть любая строка)
+// const encryptionKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
+
+// // Шифруем токен с использованием AES
+// const encryptedToken = AES.encrypt(token, encryptionKey).toString();
+
+
+const CryptoJS = require('crypto-js');
+
+// Ваш токен от Web3Forms
+const token = "f82c6329-b6b3-40be-8c6b-087136429e11";
+
+// Ключ для шифрования (может быть любая строка)
+const encryptionKey = "strong_random_key_here";
+
+// Шифруем токен с использованием AES
+const encryptedToken = CryptoJS.AES.encrypt(token, encryptionKey).toString();
+
+// Теперь вы можете отправить encryptedToken на сервер

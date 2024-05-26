9fa9f66ddb31e3440268cecf42e2ff94f29c1135
Alexandr Kavaliou <80467603+KavaliouAlexandr@users.noreply.github.com>
2024-05-26 14:55:11 +0200

    Fibra

--- html/script.js
+++ html/script.js
@@ -21,27 +21,5 @@ document.querySelector(".contact-form").addEventListener("submit", function(even
     this.reset();
 });
 
-// const AES = require('crypto-js/aes');
-
-// // Ваш токен от Web3Forms
-// const token = "f82c6329-b6b3-40be-8c6b-087136429e11";
-
-// // Ключ для шифрования (может быть любая строка)
-// const encryptionKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
-
-// // Шифруем токен с использованием AES
-// const encryptedToken = AES.encrypt(token, encryptionKey).toString();
 
 
-const CryptoJS = require('crypto-js');
-
-// Ваш токен от Web3Forms
-const token = "f82c6329-b6b3-40be-8c6b-087136429e11";
-
-// Ключ для шифрования (может быть любая строка)
-const encryptionKey = "strong_random_key_here";
-
-// Шифруем токен с использованием AES
-const encryptedToken = CryptoJS.AES.encrypt(token, encryptionKey).toString();
-
-// Теперь вы можете отправить encryptedToken на сервер
